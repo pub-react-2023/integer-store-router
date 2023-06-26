b28a6deda6c96df48b4102aaf5e5d0821cb084d2
Romi Kusuma Bakti <romikusumab@gmail.com>
2023-06-26 18:42:26 +0700

    migrate to RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <footer>&copy; 2023 IntegerStore</footer>
-    </BrowserRouter>
+      <Outlet />
+      <Footer />
+    </>
   );
 }
